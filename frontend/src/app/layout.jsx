a87f14cc36d2908c1a6f2efa88fcbadf8ccc10a5
mh-anwar <mohammad.anwar2939@gmail.com>
2024-06-08 09:36:56 -0400

    Remove NEAR wallet integration

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,21 +1,14 @@
 'use client';
 import { Navigation } from '@/components/Navigation';
-import React, { createContext, useState } from 'react';
 import '@/app/globals.css';
-
-export const WalletContext = createContext();
 
 // Layout Component
 export default function RootLayout({ children }) {
-	const [wallet, setWallet] = useState();
-
 	return (
 		<html lang='en'>
 			<body className='flex flex-col'>
-				<WalletContext.Provider value={{ wallet, setWallet }}>
-					<Navigation />
-					{children}
-				</WalletContext.Provider>
+				<Navigation />
+				{children}
 			</body>
 		</html>
 	);
